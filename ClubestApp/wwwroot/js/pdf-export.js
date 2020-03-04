c2d8d61ed5a2c4879502ad479b31d0c5f759c05a
Svetloslav15 <sn_novoselski@abv.bg>
2020-03-04 22:50:55 +0200

    feat(Polls): add pdf export to polls results

--- ClubestApp/wwwroot/js/pdf-export.js
+++ ClubestApp/wwwroot/js/pdf-export.js
@@ -12,3 +12,18 @@
         }
     });
 });
+
+$(".export-poll-btn-pdf").on("click", function () {
+    html2canvas($(this).parent()[0], {
+        onrendered: function (canvas) {
+            var data = canvas.toDataURL();
+            var docDefinition = {
+                content: [{
+                    image: data,
+                    width: 500
+                }]
+            };
+            pdfMake.createPdf(docDefinition).download("Polls-Results.pdf");
+        }
+    });
+});
