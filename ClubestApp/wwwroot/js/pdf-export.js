24ff1ebf8fcaa748a594a82b03e097ba00ed6d3e
Svetloslav15 <sn_novoselski@abv.bg>
2020-03-04 22:23:54 +0200

    feat(Events): add pdf export to events

---
+++ ClubestApp/wwwroot/js/pdf-export.js
@@ -0,0 +1,14 @@
+﻿$("#export-btn-pdf").on("click", function () {
+    html2canvas($('#my-events-table')[0], {
+        onrendered: function (canvas) {
+            var data = canvas.toDataURL();
+            var docDefinition = {
+                content: [{
+                    image: data,
+                    width: 500
+                }]
+            };
+            pdfMake.createPdf(docDefinition).download("MyEvents.pdf");
+        }
+    });
+});
